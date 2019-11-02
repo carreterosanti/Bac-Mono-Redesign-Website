2aca0402dda7a2e6ae897fa0b8564841dd2d5acb
carreterosanti <unicen123>
2019-11-02 00:46:39 -0300

    Updates

---
+++ precache-manifest.ea74a80cd6ad24dbf453a425ab4125d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a702690abc46b76cca89d1f0dbb5fcf0",
+    "url": "/bac-mono-redesign-web/index.html"
+  },
+  {
+    "revision": "344c54eab9fac1d056d5",
+    "url": "/bac-mono-redesign-web/static/css/main.a274f844.chunk.css"
+  },
+  {
+    "revision": "64c5c7fc245d0f052ec2",
+    "url": "/bac-mono-redesign-web/static/js/2.2af61d84.chunk.js"
+  },
+  {
+    "revision": "344c54eab9fac1d056d5",
+    "url": "/bac-mono-redesign-web/static/js/main.ba1397a6.chunk.js"
+  },
+  {
+    "revision": "d2501aa55e69aaa9a783",
+    "url": "/bac-mono-redesign-web/static/js/runtime-main.7faf1133.js"
+  }
+]);
